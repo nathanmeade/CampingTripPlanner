dcd1bbe779bb63fe851dcd219f1d17ad43f5250b
Nathan <nathan.r.meade@gmail.com>
2019-09-16 12:10:41 -0400

    feat: Create trip activity should be finished

--- app/src/main/java/com/example/campingtripplanner/CreateTripActivity.java
+++ app/src/main/java/com/example/campingtripplanner/CreateTripActivity.java
@@ -1,14 +1,25 @@
 package com.example.campingtripplanner;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.PorterDuff;
+import android.os.Build;
 import android.os.Bundle;
 
 public class CreateTripActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_trip);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.getNavigationIcon().setColorFilter(ContextCompat.getColor(this, R.color.colorAccent), PorterDuff.Mode.SRC_IN);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        /*getSupportActionBar().setTitle("String");*/
     }
 }
