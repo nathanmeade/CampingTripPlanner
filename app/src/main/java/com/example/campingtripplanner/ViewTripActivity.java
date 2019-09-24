448251fbded2c61665a393db761c1f2aec6a658a
Nathan Meade <nathan.r.meade@gmail.com>
2019-09-23 20:12:22 -0400

    feat: Google maps intent working

--- app/src/main/java/com/example/campingtripplanner/ViewTripActivity.java
+++ app/src/main/java/com/example/campingtripplanner/ViewTripActivity.java
@@ -7,7 +7,11 @@ import androidx.room.Room;
 
 import android.content.Intent;
 import android.graphics.PorterDuff;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -15,6 +19,7 @@ import org.w3c.dom.Text;
 public class ViewTripActivity extends AppCompatActivity {
 
     private AppDatabase db;
+    private Trip trip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +33,7 @@ public class ViewTripActivity extends AppCompatActivity {
         int tid = intent.getIntExtra("tid", 0);
         db = Room.databaseBuilder(getApplicationContext(),
                 AppDatabase.class, "database-name").allowMainThreadQueries().build();
-        Trip trip = db.tripDao().findByTid(tid);
+        trip = db.tripDao().findByTid(tid);
         TextView nameTextView = findViewById(R.id.nameTextView);
         TextView locationTextView = findViewById(R.id.locationTextView);
         TextView dateRangeTextView = findViewById(R.id.dateRangeTextView);
@@ -38,5 +43,22 @@ public class ViewTripActivity extends AppCompatActivity {
         TextView baconTextView = findViewById(R.id.baconTextView);
 
         nameTextView.setText(trip.name);
+        locationTextView.setText(trip.location);
+        dateRangeTextView.setText(trip.arrival + " - " + trip.departure);
+        tentTextView.setText(trip.tent + "x Tent");
+        bagTextView.setText(trip.bag + "x Sleeping Bag");
+        eggsTextView.setText(trip.eggs + "x Eggs");
+        baconTextView.setText(trip.bacon + "x Bacon");
+
+        ImageView viewLocationButton = findViewById(R.id.viewLocationButton);
+        viewLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse("geo:" + trip.location);
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
     }
 }
