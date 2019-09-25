ee03dbb483d2c491f2128aa5ff26dfc633d19501
Nathan Meade <nathan.r.meade@gmail.com>
2019-09-25 17:21:42 -0400

    feat: Open weather api added

--- app/src/main/java/com/example/campingtripplanner/ViewTripActivity.java
+++ app/src/main/java/com/example/campingtripplanner/ViewTripActivity.java
@@ -16,6 +16,12 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ViewTripActivity extends AppCompatActivity {
 
     private AppDatabase db;
@@ -65,6 +71,36 @@ public class ViewTripActivity extends AppCompatActivity {
                 Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
                 mapIntent.setPackage("com.google.android.apps.maps");
                 startActivity(mapIntent);
+            }
+        });
+        String location = trip.location;
+        String coordinates[] = location.split(",");
+        String lat = "34.0767";
+        String lon = "-82.8547";
+        final TextView weatherTextView = findViewById(R.id.weatherTextView);
+        String appid = BuildConfig.openweatherkey;
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.openweathermap.org/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        Call<Weather> call = jsonPlaceHolderApi.getWeather(coordinates[0], coordinates[1], appid);
+        call.enqueue(new Callback<Weather>() {
+            @Override
+            public void onResponse(Call<Weather> call, Response<Weather> response) {
+                if (!response.isSuccessful()){
+                    return;
+                }
+                Weather weather = response.body();
+                Main main = weather.getMain();
+                double temp = main.getTemp();
+                temp = (((temp - 273) * 9/5) + 32);
+                weatherTextView.setText(Double.toString(temp));
+            }
+
+            @Override
+            public void onFailure(Call<Weather> call, Throwable t) {
+
             }
         });
 
---
+++ app/src/main/java/com/example/campingtripplanner/Weather.java
@@ -0,0 +1,9 @@
+package com.example.campingtripplanner;
+
+import java.util.ArrayList;
+
+class Weather {
+    private Main main;
+
+    public Main getMain(){return main;}
+}
