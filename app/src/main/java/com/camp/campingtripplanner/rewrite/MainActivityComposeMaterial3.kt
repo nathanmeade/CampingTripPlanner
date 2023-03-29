32fd5a081ab561989863780bc75f15a2e16fcdb4
nathanmeade <nathan.r.meade@gmail.com>
2023-03-29 13:48:35 -0400

    Imports optimized

--- app/src/main/java/com/camp/campingtripplanner/rewrite/MainActivityComposeMaterial3.kt
+++ app/src/main/java/com/camp/campingtripplanner/rewrite/MainActivityComposeMaterial3.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.camp.campingtripplanner.ui.theme.CampingTripPlannerTheme
 
 //@AndroidEntryPoint
 class MainActivityComposeMaterial3 : ComponentActivity() {
