3dcb441714f0af71dc16c2a1968b0d263ef85b69
nathanmeade <nathan.r.meade@gmail.com>
2023-03-29 13:44:06 -0400

    MainActivityComposeMaterial3.kt screen created

--- app/src/main/java/com/camp/campingtripplanner/rewrite/MainActivityComposeMaterial3.kt
+++ app/src/main/java/com/camp/campingtripplanner/rewrite/MainActivityComposeMaterial3.kt
@@ -3,43 +3,66 @@ package com.camp.campingtripplanner.rewrite
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.camp.campingtripplanner.ui.theme.CampingTripPlannerTheme
-import dagger.hilt.android.AndroidEntryPoint
 
 //@AndroidEntryPoint
 class MainActivityComposeMaterial3 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CampingTripPlannerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+            content()
+        }
+    }
+}
+
+@Composable
+fun content() {
+    MaterialTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background,
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(
+                    onClick = { /*TODO*/ }
                 ) {
-                    Greeting("Android")
+                    Text(
+                        text = "CREATE TRIP",
+                        fontSize = 40.sp
+                    )
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    onClick = { /*TODO*/ }
+                ) {
+                    Text(
+                        text = "SELECT TRIP",
+                        fontSize = 40.sp
+                    )
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    CampingTripPlannerTheme {
-        Greeting("Android")
-    }
+    content()
 }
