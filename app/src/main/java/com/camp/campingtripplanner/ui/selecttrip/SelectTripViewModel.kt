03e6ee638e2aa331a53996faede1bd509e6b211d
nathanmeade <nathan.r.meade@gmail.com>
2023-03-31 18:31:14 -0400

    CreateTrip and SelectTrip ViewModels created

---
+++ app/src/main/java/com/camp/campingtripplanner/ui/selecttrip/SelectTripViewModel.kt
@@ -0,0 +1,9 @@
+package com.camp.campingtripplanner.ui.selecttrip
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SelectTripViewModel @Inject constructor() : ViewModel() {
+}
