228c669e306d54f76031cd9fa51c083d58dd5d12
nathanmeade <nathan.r.meade@gmail.com>
2023-03-31 00:27:35 -0400

    CampingTripPlannerApplication.kt created

---
+++ app/src/main/java/com/camp/campingtripplanner/CampingTripPlannerApplication.kt
@@ -0,0 +1,8 @@
+package com.camp.campingtripplanner
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CampingTripPlannerApplication : Application() {
+}
