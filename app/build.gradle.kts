5f5fd7a273700f6e7bbc885d87961232180209e0
Nathan Meade <nathan.r.meade@gmail.com>
2025-04-27 20:13:59 -0400

    ongoing attempts to restore github action to upload to Google Play

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.camp.campingtripplanner"
         minSdk = 19
         targetSdk = 33
-        versionCode = 7
-        versionName = "1.6"
+        versionCode = 8
+        versionName = "1.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
